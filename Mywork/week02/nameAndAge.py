8ac6a0d4101d4948a9956514b6ba3e7f343f26f5
Tatjana <g00425678@atu.ie>
2023-02-12 21:09:25 +0100

    nameAndAge.py

---
+++ Mywork/week02/nameAndAge.py
@@ -0,0 +1,6 @@
+# nameAndAge.py
+# This program reads in a name and age
+# Author: Tatjana Staunton
+name = input ("Enter your name: ")
+age = int(input ("Please enter your age: "))
+print (f"Hello {name}, \t your age is {age} ")
