4018084776a4998ec91bf5d86864e390f5d81985
Tatjana <g00425678@atu.ie>
2023-02-12 20:43:18 +0100

    addOne.py

---
+++ Mywork/week02/addOne.py
@@ -0,0 +1,6 @@
+# addOne.py
+# This program reads on a number and prints out one more than that number
+# Author: Tatjana Staunton
+number = int(input ("please enter a number: "))
+newNumber = number + 1
+print (f" {number} plus one is {newNumber} ")
