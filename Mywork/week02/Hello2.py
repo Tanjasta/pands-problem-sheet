d4f1e5d59968a6ccec4f6285ac7de5f05170b35d
Tatjana <g00425678@atu.ie>
2023-02-12 20:18:19 +0100

    Hello2.py

---
+++ Mywork/week02/Hello2.py
@@ -0,0 +1,6 @@
+# Hello2.py
+# This program reads in a person’s name and prints out Hello that persons name
+# Author: Tatjana Staunton
+name = input ("Enter your name: ") 
+print ("Hello " + name + " \nNice to meet you ")
+print (f"Hello { name } \nNice to meet you ")
