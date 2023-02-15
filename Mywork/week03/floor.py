184dad293d61aae987f4171d2440488e603d88d5
Tatjana <g00425678@atu.ie>
2023-02-15 19:30:55 +0100

    floor.py

---
+++ Mywork/week03/floor.py
@@ -0,0 +1,8 @@
+# floor.py
+# This program takes in a float and outputs an rounded down int
+# Author: Tatjana Staunton
+
+import math
+numberTofloor = float (input ("Enter a float number:"))
+flooredNumber = math .floor (numberTofloor)
+print (" {} floored is {}" .format (numberTofloor, flooredNumber))
