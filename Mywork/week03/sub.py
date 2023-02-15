06e913bf43bba1486a2d2cd9fed89af756a5ce58
Tatjana <g00425678@atu.ie>
2023-02-15 17:54:30 +0100

    sub.py

---
+++ Mywork/week03/sub.py
@@ -0,0 +1,10 @@
+# sub.py
+# Author: Tatjana Staunton
+# That reads in two numbers and subtracts the first number from the second one
+# Input reads as string and needs to be converted into an int
+# So maths can be performed
+
+x = int (input ("Enter first number: "))
+y = int (input ("Enter second number: "))
+answer = x-y
+print (" {} minus {} is {} " .format (x, y, answer))
