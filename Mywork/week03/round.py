a7e7617ab2bb67aa1e50cf47a38ca747a1d3d668
Tatjana <g00425678@atu.ie>
2023-02-15 19:04:29 +0100

    round.py

---
+++ Mywork/week03/round.py
@@ -0,0 +1,7 @@
+# round.py
+# This program takes in a float and output an int (rounded up or down)
+# Author: Tatjana Staunton
+
+numberToRound = float (input("Enter a float number:"))
+roundedNumber = round (numberToRound)
+print (" {} rounded is {}" .format (numberToRound, roundedNumber))
