2a2542f86fe5ce8724b516c86ace34311f2f4d2e
Tatjana <g00425678@atu.ie>
2023-02-15 19:18:11 +0100

    absolute.py

---
+++ Mywork/week03/absolute.py
@@ -0,0 +1,7 @@
+# absolute.py
+# This program takes in number and give its absolute value
+# Author: Tatjana Staunton
+
+number = float (input ("Enter a number:" ))
+absoluteValue = abs (number)
+print ("the absolute value of {} is {}" .format (number, absoluteValue))
