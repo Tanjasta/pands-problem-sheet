745ae7cea8c1ef7b254b432894ae012a9d8a72de
Tatjana <g00425678@atu.ie>
2023-02-15 19:59:03 +0100

    length.py

---
+++ Mywork/week03/length.py
@@ -0,0 +1,11 @@
+# length.py
+# This program reads in a string and outputs how long it is
+
+inputString = input('enter a string:')
+lenghtOfString = len(inputString)
+print(f'The lenght of {inputString} is {lenghtOfString }')
+
+
+
+message = 'John said\t"hi"\nI said\t\t"bye"'
+print (message)
