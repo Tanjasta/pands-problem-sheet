ab12d142edefc83c298563bd97a4c8ceb612b6db
Tatjana <g00425678@atu.ie>
2023-02-15 18:25:17 +0100

    randomGenerator.py

---
+++ Mywork/week03/randomGenerator.py
@@ -0,0 +1,7 @@
+# randomGenerator.py
+# author: Tatjana Staunton
+# This program prints out a random number between 1 and 10
+
+import random 
+number = random. randint (1, 10)
+print ("here is a random number {}" .format (number))
