b320aa7ef1f49f427c9fa5fa1d36cdf7931698da
Tatjana <g00425678@atu.ie>
2023-03-09 21:35:23 +0100

    comments added

--- Weekly_Tasks/weekday.py
+++ Weekly_Tasks/weekday.py
@@ -4,8 +4,8 @@
 
 day = (input ("Please enter today's Day: ")) # input from user to type a current day
 day = [ "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday", "Sunday"] # List of possible days for input
-for element in day: # For chosen day program will print that day
-    print (element)
+for element in day: 
+    print (element) # Program prints list of days
 weekday = ["Monday", "Tuesday", "Wendsday", "Thursday", "Friday"] #list of weekdays
 for wd in weekday:
     print (wd)
