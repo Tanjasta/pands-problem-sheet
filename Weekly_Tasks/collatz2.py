2bd4d16f118dbd4fc6c4367852188aab64654b7b
Tatjana <g00425678@atu.ie>
2023-03-09 21:31:07 +0100

    comments added

---
+++ Weekly_Tasks/collatz2.py
@@ -0,0 +1,8 @@
+number = int (input ("Please enter a positive number"))
+if number % 2 ==0:
+    print (int (number))
+    print (int (number / 2))
+else:
+    if number % 2 != 0:
+        print (int (number))
+        print (int (number * 3 +1 ))
--- Weekly_Tasks/weekday.py
+++ Weekly_Tasks/weekday.py
@@ -3,9 +3,9 @@
 # Author: Tatjana Staunton
 
 day = (input ("Please enter today's Day: ")) # input from user to type a current day
-day = [ "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday", "Sunday"]
-for element in day:
+day = [ "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday", "Sunday"] # List of possible days for input
+for element in day: # For chosen day program will print that day
     print (element)
-weekday = ["Monday", "Tuesday", "Wendsday", "Thursday", "Friday"]
+weekday = ["Monday", "Tuesday", "Wendsday", "Thursday", "Friday"] #list of weekdays
 for wd in weekday:
     print (wd)
