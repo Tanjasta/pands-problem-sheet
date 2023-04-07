a358705d8358e806e7d0ecd6bedbc511ba2a3c95
Tatjana <g00425678@atu.ie>
2023-04-07 14:08:23 +0200

    comments added

--- Weekly_Tasks/es.py
+++ Weekly_Tasks/es.py
@@ -1,3 +1,10 @@
 # This program reads in a text file and outputs the number of e's it contains
 # It takes the filename from an argument on the command line
 # Author: Tatjana Staunton
+
+
+FILENAME = "data.txt"
+with open (FILENAME, 'rt') as f:
+    for data in f:
+        print(data)
+    
