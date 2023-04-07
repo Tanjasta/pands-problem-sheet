d9f43cb70339267f87d6f8d61184076b0ddf782a
Tatjana <g00425678@atu.ie>
2023-04-07 20:02:03 +0200

    changes made to the program

--- README.md
+++ README.md
@@ -90,8 +90,8 @@ Referense for the data.txt file is https://the-big-bang-theory.com/soft_kitty/
 
 *References*
 https://web.microsoftstream.com/video/a2ba3a79-e22c-4997-9362-ad558fca31ef?referrer=https:%2F%2Fvlegalwaymayo.atu.ie%2F
-https://www.geeksforgeeks.org/python-sys-module/
-https://www.tutorialspoint.com/python/python_command_line_arguments.htm#
+https://www.w3schools.com/python/python_file_open.asp
+https://realpython.com/working-with-files-in-python/
 
 
 
--- Weekly_Tasks/es.py
+++ Weekly_Tasks/es.py
@@ -3,8 +3,13 @@
 # Author: Tatjana Staunton
 
 # created small txt file named data.txt
-FILENAME = "data.txt" # defined FILENAME
-with open (FILENAME, 'rt') as f:
+'''FILENAME = "data.txt" # defined FILENAME
+with open (FILENAME, 'rt') as f: 
     for data in f:
-        print(data)
-    
+        print(data) # read file and print out the content
+        '''
+filename = input("Please enter file name:") #asking user to input name of the file for program to read
+
+with open (filename, 'rt') as f: 
+    for data in f:
+        print("The data in the file is: "+ data) # read file and print out the content
